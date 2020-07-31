1b6da365fd0256476f3b3c0d6280922621502dc0
Ontefetse-Ditsele <ontefetse.ditsele@outlook.com>
2020-07-31 21:10:30 +0200

    Made Changes to DamArrayApp, editing the code, removing unsed variables

--- Question_5/src/DamArrayApp.java
+++ Question_5/src/DamArrayApp.java
@@ -3,50 +3,47 @@
  *29 July 2020
 */
 
-package Unix.Question_5;
+//package Unix.Question_5;
+
 
 import java.io.*;
-//import java.util.*;
 
 public class DamArrayApp{
    private DamArrayApp(){}
    
-   private static String path = "C:/Users/connex/OneDrive/Desktop/Zaio_deliverables/Unix/Question_5/dam_levels.csv";
-   private static String nFilePath = "C:/Users/connex/OneDrive/Desktop/Zaio_deliverables/Unix/Question_5/dam_levels.txt";
+   private static String path = "C:/Users/connex/OneDrive/Desktop/Zaio_deliverables/Unix/Question_5/bin/dam_levels.csv";
+   private static String nFilePath = "C:/Users/connex/OneDrive/Desktop/Zaio_deliverables/Unix/Question_5/bin/dam_levels.txt";
    
    private static String[] lineValues;
-   private static String  FileContents = "";
-   static String line = "";//static String fscLevel = "";static String damLevel = ""; 
+   private static String line = ""; 
 
    
    private static BufferedReader read;
    private static FileWriter  write; 
       
-   public static void main(String[] args){
+   public static void main(final String[] args){
       try{
          read = new BufferedReader(new FileReader(path));
          write = new FileWriter(nFilePath,true);
-         
-            
       
-      if(args.length > 0){ printDam(args[0]);}
+         if(args.length > 0){ printDam(args[0]); }
+         else{ printAll(); }
       
-      else{/* printAll();*/ /*printDam("Pella Dam");  printDam("Madikwe Dam"); *printDam("Molatedi Dam");*/ printDam("Saulspoort Dam");}
+   
+         write.close();
       
-      write.write(FileContents);
-      write.close();
-      
-      }catch(FileNotFoundException e){ System.out.println("File does not exist"); }
-       catch(IOException e){ System.out.println("What file now");  }
+      }catch(final FileNotFoundException e){ System.out.println("File does not exist"); }
+       catch(final IOException e){ System.out.println("What file now");  }
  
    }
    
-   public static void printDam(String damName){
+   public static void printDam(final String damName){
       String tName; String oName;     
       try{
          while((line = read.readLine()) != null){
             lineValues = line.split(",");
             
+            //Cleaning data to make it easier to compare dam Names.
             tName      = lineValues[2];            
             tName = tName.replace(" ",""); tName = tName.substring(1, tName.length() -1);
             oName = damName.replace(" ","");
@@ -55,19 +52,15 @@ public class DamArrayApp{
                if(lineValues.length >= 25){
                  
                   write.write("Dam Name: "+damName +"\nFSC: "+lineValues[10]+"\nDam Level: "+lineValues[25]+"\n\n");
-               }
-               //else{ System.out.println(damName +" Have missing Values."); }
+               }  
             }
-            //else{ System.out.println("Dam Not Found"); }
-            
          }
       
-      }catch(IOException e){
-         System.out.println("Could not read line");}
+      }catch(final IOException e){ System.out.println("Could not read line"); }
    }
    
    public static void printAll(){
-   String dam = "";
+      String dam = "";
       try{
          while((line = read.readLine()) != null){
             lineValues = line.split(",");
@@ -77,11 +70,9 @@ public class DamArrayApp{
                if(dam.contains("Dam")){
                   write.write(lineValues[2] +"   "+lineValues[10]+"   "+lineValues[25]+"\n");
                }
-               //else{ System.out.println("Wrong values"); }
-            }
-            //else{ System.out.println("Row have missing values"); } 
+            } 
          }
-       }catch(IOException e){
+       }catch(final IOException e){
             System.out.println("Could not read line");}
       
       }      
